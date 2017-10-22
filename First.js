ef4e47c81896e01ba8a901fa69506e08a3da4855
Paul <paul.caloianu@gmail.com>
2017-10-22 07:34:26 +0200

    3rd commit

--- First.js
+++ First.js
@@ -1,9 +1,5 @@
-// Declare an anonymous function
-(function () {
-    var foo = 42;
-    console.log(window.foo);
-    // → undefined
-    console.log(foo);
-    // → 42
-})();
-//^ and call it immediately
+(function (global, doc) {
+    global.setTimeout(function () {
+        doc.body.innerHTML = "Hello!";
+    }, 1000);
+})(window, document);
